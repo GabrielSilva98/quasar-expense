adcff6203f69db6d7e00140f387ccbaaac6f9042
GabrielSilva98 <gabriel8198@gmail.com>
2022-12-14 21:27:26 -0300

    Quasar estoque system

---
+++ src/composables/UseNotify.js
@@ -0,0 +1,25 @@
+
+import { useQuasar } from 'quasar'
+
+export default function useNotify () {
+  const $q = useQuasar()
+
+  const notifySuccess = (message) => {
+    $q.notify({
+      type: 'positive',
+      message: message || 'All right'
+    })
+  }
+
+  const notifyError = (message) => {
+    $q.notify({
+      type: 'negative',
+      message: message || 'Failded'
+    })
+  }
+
+  return {
+    notifySuccess,
+    notifyError
+  }
+}
